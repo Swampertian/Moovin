890ddda1708da0961e897c14bee29472245f8c56
Mayconncs <mayconncardoso2015@gmail.com>
2025-04-13 18:39:48 -0300

    Frontend do perfil do inquilino

---
+++ Mobile/rental_app/lib/services/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/tenant.dart';
+
+class ApiService {
+  static const String baseUrl = 'http://127.0.0.1:8000/api';
+
+  Future<Tenant> fetchTenant(int id) async {
+    final response = await http.get(Uri.parse('$baseUrl/tenants/$id/'));
+
+    if (response.statusCode == 200) {
+      return Tenant.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load tenant profile');
+    }
+  }
+  
+  Future<void> favoriteProperty(int tenantId) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/tenants/$tenantId/favorite_property/'),
+    );
+    
+    if (response.statusCode != 200) {
+      throw Exception('Failed to mark property as favorite');
+    }
+  }
+}
