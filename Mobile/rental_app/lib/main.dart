0f7f792926e5c84401f23b784372d6a1a0fbbdec
Ana4Julia <anajuliacv8@hotmail.com>
2025-04-26 20:12:04 -0300

    Resolvendo conflitos

--- Mobile/rental_app/lib/main.dart
+++ Mobile/rental_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/tenant_profile_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +17,14 @@ class MyApp extends StatelessWidget {
       title: 'Rental App',
       theme: ThemeData(
         primarySwatch: Colors.green,
+        textTheme: GoogleFonts.khulaTextTheme(), 
       ),
-      home: const TenantProfileScreen(tenantId: 2),
+      home: const LoginScreen(),
+      //home: const TenantProfileScreen(tenantId: 2),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+      },
     );
   }
 }
