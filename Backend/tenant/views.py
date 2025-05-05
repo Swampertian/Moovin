e772411f15d09d9eef7666244b86e0901483a61f
VicthorCM <vcabralmagalhaes@gmail.com>
2025-05-05 10:23:05 -0300

    backend

--- Backend/tenant/views.py
+++ Backend/tenant/views.py
@@ -1,6 +1,6 @@
 
 from rest_framework import generics, status
-
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
@@ -11,6 +11,8 @@ from django.contrib.auth.models import User
 class TenantViewSet(ModelViewSet):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
+    permission_classes=[IsAuthenticated]
+
     @action(detail=True, methods=['patch'], url_path='update-profile')
     def update_profile(self, request, pk=None):
         tenant = self.get_object()
@@ -19,4 +21,15 @@ class TenantViewSet(ModelViewSet):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=False, methods=['get'], url_path='me')
+    def me(self,request):
+        try:
+            profile = Tenant.objects.get(user=request.user)
+        except Tenant.DoesNotExist:
+            return Response({"detail": "Perfil não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data)
+
 
