c8d43137661ae86f02240564212fc3f53afc4014
VicthorCM <vcabralmagalhaes@gmail.com>
2025-05-26 14:20:29 -0300

    ajustes finais

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -9,20 +9,24 @@ from rest_framework.views import APIView
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     ordering = ['email']
-    list_display = ['email', 'name', 'user_type', 'is_staff']
+    list_display = ['email', 'username', 'name', 'user_type', 'is_staff']
+    
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'username', 'password')}),
         ('Informações pessoais', {'fields': ('name', 'user_type')}),
         ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Datas importantes', {'fields': ('last_login',)}),
     )
+    
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'user_type', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'username', 'name', 'user_type', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
-    search_fields = ('email', 'name')
+    
+    search_fields = ('email', 'username', 'name')
+
 
 
 # Essa classe serve para controlar a permissao de todos os usuarios. apenas admins conseguem acessar os dados de todos os usuarios.
